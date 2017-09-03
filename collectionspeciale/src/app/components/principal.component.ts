931c9c6ff749b0bb2ff821c084246018197aa4c3
kevin_takla@hotmail.com <kevin_takla@hotmail.com>
2017-09-03 16:02:55 -0400

    gabarit html fait

--- collectionspeciale/src/app/components/principal.component.ts
+++ collectionspeciale/src/app/components/principal.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'principal-component',
-  templateUrl: '../pages/html/principal.html'/*,
-  styleUrls: ['../pages/css/principal.css']*/
+  templateUrl: '../pages/html/principal.html',
+  styleUrls: ['../pages/css/principal.css']
 })
 export class PrincipalComponent {}
